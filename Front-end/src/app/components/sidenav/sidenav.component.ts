1c111fadf2c18dff0f325ece94e414378815f86c
Hope Tsunke <ttsunke15@gmail.com>
2024-08-28 16:19:47 +0200

    chore: Update header and sidenav components

--- Front-end/src/app/components/sidenav/sidenav.component.ts
+++ Front-end/src/app/components/sidenav/sidenav.component.ts
@@ -1,7 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  RouterLink,
+  RouterOutlet,
+  Router,
+  NavigationEnd,
+} from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthService } from '../../services/auth/auth.service';
+import { filter } from 'rxjs';
 import {
   faNewspaper,
   faHourglass,
@@ -13,17 +20,32 @@ import {
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [FontAwesomeModule, RouterOutlet, RouterLink],
+  imports: [FontAwesomeModule, RouterOutlet, RouterLink, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css',
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
   authservice = inject(AuthService);
-
-  buttonStyle: string = ' rounded-md w-full py-2 flex flex-row gap-3 px-3';
+  activeLink: string = '';
+  inactiveStyle: string = 'rounded-md w-full py-2 flex flex-row gap-3 px-3';
+  activeStyle: string =
+    this.inactiveStyle + ' bg-dashboard_bg text-purple_accent';
   faNewspaper = faNewspaper;
   faHourglass = faHourglass;
   faListCheck = faListCheck;
   faThumbTack = faThumbTack;
   faUsers = faUsers;
+
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.activeLink = event.url;
+      });
+  }
+
+  isActive(path: string): boolean {
+    return this.activeLink === path;
+  }
 }
