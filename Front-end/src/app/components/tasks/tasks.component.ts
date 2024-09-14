29b2c90d52a4ed8f1d16a35a1d6c9537bbe62a47
Hope Tsunke <ttsunke15@gmail.com>
2024-09-14 06:44:12 +0200

    refactor: Update file upload accept attribute
    The accept attribute in the file input element of the editprofile.component.html file was modified to restrict the file types to .jpg, .jpeg, and .png only. This change improves the user experience by ensuring that only valid image files can be selected for profile pictures.

--- Front-end/src/app/components/tasks/tasks.component.ts
+++ Front-end/src/app/components/tasks/tasks.component.ts
@@ -3,24 +3,29 @@ import { TasksService } from '../../services/tasks/tasks.service';
 import { AuthService } from '../../services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, NzTableModule],
+  imports: [CommonModule, NzTableModule, FontAwesomeModule, FormsModule],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css',
+  styleUrls: ['./tasks.component.css'], // Fixed `styleUrls` property name
 })
 export class TasksComponent {
   taskService = new TasksService();
   authService = new AuthService();
   user: any;
-  tasks: any;
+  tasks: any[] = [];
+  searchQuery: string = '';
+  faFileArrowDown = faFileArrowDown;
 
   constructor() {
     this.user = this.authService.getCurrentUser();
     this.taskService.getUserTasks(this.user.id).subscribe((data) => {
-      //split day from time
+      // Split day from time
       data.forEach((d: any) => {
         d.DueDate = d.DueDate.split('T')[0];
       });
@@ -28,12 +33,19 @@ export class TasksComponent {
     });
   }
 
+  get filteredTasks() {
+    if (!this.searchQuery) {
+      return this.tasks;
+    }
+    return this.tasks.filter((task) =>
+      task.Name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
   setStatus(task: any, status: string) {
     task.Status = status;
     this.taskService.setProgress(task.Task_id, status).subscribe((data) => {
-      // this.taskService.getUserTasks(this.user.id).subscribe((data) => {
-      //   this.tasks = data;
-      // });
+      // You might want to update the task list here if needed
     });
   }
 }
